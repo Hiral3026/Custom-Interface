5448f4abc6310159fed4c2f9aaafefeacb39f48a
Hiral Patel <hiral@hiralpatel3026@gmail.com>
2024-06-18 14:39:33 -0400

    Initial commit

---
+++ products.js
@@ -0,0 +1,50 @@
+﻿//LAB 9-DATA STORAGE: PRODUCTS PAGE
+
+window.onload = function() {
+            
+    if(localStorage.getItem('name') && localStorage.getItem('color')){
+      var storedName = localStorage.getItem('name');
+      var storedColor = localStorage.getItem('color');
+      
+     
+      document.body.style.background = storedColor;
+      
+      
+      document.getElementById('newMsgBox').innerHTML = "Welcome, " + storedName;
+    }
+  
+    
+    var form = document.querySelector('form[name="infoForm"]');
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // prevent the form from submitting
+  
+      
+      var name = document.getElementById('inName').value;
+      var color = document.getElementById('inColor').value;
+  
+      
+      localStorage.setItem('name', name);
+      localStorage.setItem('color', color);
+  
+      
+      document.body.style.background = color;
+  
+      
+      document.getElementById('newMsgBox').innerHTML = "Welcome, " + name;
+    });
+  
+    
+    var btnDel = document.getElementById('btnDel');
+    btnDel.addEventListener('click', function(event) {
+      event.preventDefault(); // prevent the button from submitting
+  
+      
+      localStorage.removeItem('name');
+      localStorage.removeItem('color');
+      document.getElementById('newMsgBox').innerHTML = "Welcome!";
+      document.body.style.background = "#c0c0c0";
+    });
+  };
+
+
+  
